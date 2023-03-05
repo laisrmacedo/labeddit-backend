cd1bcffc5ae0df057d75a0f123354f5ce334705f
Lais Rodrigues <laisrmacedo@air-de-lais.speedport.ip>
2023-03-05 09:57:02 +0100

    :sparkles: feat: class user with methods

---
+++ src/models/User.ts
@@ -0,0 +1,66 @@
+import { UserBusinessModel } from "../business/UserBusiness"
+import { UserDB } from "../database/UserDatabase"
+
+export enum USER_ROLES {
+  NORMAL = "NORMAL",
+  ADMIN = "ADMIN"
+}
+
+export class User {
+  constructor(
+    private id: string, 
+    private nickname: string,
+    private email: string, 
+    private password: string,
+    private avatar: string,
+    private role: USER_ROLES,
+    private createdAt: string
+  ){}
+
+  public getId():string{
+    return this.id
+  }
+  public getNickname():string{
+    return this.nickname
+  }
+  public getEmail():string{
+    return this.email
+  }
+  public getPassword():string{
+    return this.password
+  }
+  public getAvatar():string{
+    return this.avatar
+  }
+  public getRole():USER_ROLES{
+    return this.role
+  }
+  public getCreatedAt():string{
+    return this.createdAt
+  }
+
+  public toDBModel(): UserDB {
+    return {
+      id: this.id,
+      nickname: this.nickname,
+      email: this.email,
+      password: this.password,
+      avatar: this.avatar,
+      role: this.role,
+      created_at: this.createdAt
+    }
+  }
+
+  public toBusinessModel(): UserBusinessModel {
+    return {
+      id: this.id,
+      nickname: this.nickname,
+      email: this.email,
+      password: this.password,
+      avatar: this.avatar,
+      role: this.role,
+      createdAt: this.createdAt
+    }
+  }
+
+}
