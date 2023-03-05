845b6ccd349f7bff52926ebb57442f3fe963cb87
Lais Rodrigues <laisrmacedo@air-de-lais.speedport.ip>
2023-03-05 09:20:38 +0100

    :recycle: refactor: error message

--- src/errors/BadRequestError.ts
+++ src/errors/BadRequestError.ts
@@ -2,7 +2,7 @@ import { BaseError } from "./BaseError";
 
 export class BadRequestError extends BaseError {
     constructor(
-        message: string = "Requisição inválida"
+        message: string = "Bad request."
     ) {
         super(400, message)
     }
