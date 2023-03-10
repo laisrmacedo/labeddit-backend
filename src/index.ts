d9d52836e285ee7233e57a958c25dde7189845a9
laisrmacedo <laisrodriguesmacedo@gmail.com>
2023-03-10 16:13:09 +0100

    :bug: fix: commentRouter class

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import { userRouter } from './router/userRouter'
 import { postRouter } from './router/postRouter'
+import { commentRouter } from './router/commentRouter'
 
 dotenv.config()
 
@@ -17,4 +18,4 @@ app.listen(Number(process.env.PORT), () => {
 
 app.use("/users", userRouter)
 app.use("/posts", postRouter)
-app.use("/comments", postRouter)
+app.use("/comments", commentRouter)
