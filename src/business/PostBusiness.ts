a3d7834477ac65db862d09d325816a5ace08a472
laisrmacedo <laisrodriguesmacedo@gmail.com>
2023-03-18 07:52:05 -0300

    :construction: endpoints not used in the mobile version commented out

--- src/business/PostBusiness.ts
+++ src/business/PostBusiness.ts
@@ -153,90 +153,29 @@ export class PostBusiness {
     await this.postDatabase.insertPost(newPost.toDBModel())
   }
 
-  public editPost = async (input: EditPostOutputDTO): Promise<void> => {
-    const { idToEdit, token, content } = input
-
-    const postDB = await this.postDatabase.getPostById(idToEdit)
-    if (!postDB) {
-      throw new NotFoundError("ERROR: 'idToEdit' not found.")
-    }
-
-    //login ckeck
-    const payload = this.tokenManager.getPayload(token)
-    if (payload === null) {
-      throw new BadRequestError("ERROR: Login failed")
-    }
-
-    if (postDB.creator_id !== payload.id) {
-      throw new ForbiddenError("ERROR: There's no permission to complete the request.")
-    }
-
-    //characters quantity
-    if (content.length > 280) {
-      throw new BadRequestError("ERROR: The maximum post length is 280 characters.")
-    }
-
-    const newPost = new Post(
-      postDB.id,
-      postDB.creator_id,
-      postDB.content,
-      postDB.upvote,
-      postDB.downvote,
-      postDB.comments,
-      postDB.created_at,
-      postDB.updated_at
-    )
-
-    newPost.setContent(content)
-    newPost.setUpdatedAt(new Date().toISOString())
-
-    await this.postDatabase.updatePost(idToEdit, newPost.toDBModel())
-  }
-
-  public deletePost = async (input: DeletePostOutputDTO): Promise<void> => {
-    const { idToDelete, token } = input
-
-    const postDB: PostDB | undefined = await this.postDatabase.getPostById(idToDelete)
-    if (!postDB) {
-      throw new NotFoundError("ERROR: 'idToDelete' not found.")
-    }
-
-    //login ckeck
-    const payload = this.tokenManager.getPayload(token)
-    if (payload === null) {
-      throw new BadRequestError("ERROR: Login failed.")
-    }
-
-    //permission check
-    if (payload.role !== USER_ROLES.ADMIN && postDB.creator_id !== payload.id) {
-      throw new ForbiddenError("ERROR: There's no permission to complete the request.")
-    }
-
-    await this.postDatabase.deletePost(idToDelete)
-  }
-
+  
   public upvoteOrDownvotePost = async (input: UpvoteOrDownvotePostOutputDTO): Promise<void> => {
     const { idToVote, token, vote } = input
-
+    
     const postDB: PostDB | undefined = await this.postDatabase.getPostById(idToVote)
     if (!postDB) {
       throw new NotFoundError("ERROR: 'idToVote' not found")
     }
-
+    
     //login ckeck
     const payload = this.tokenManager.getPayload(token)
     if (payload === null) {
       throw new BadRequestError("ERROR: Login failed")
     }
-
+    
     const voteDB = vote ? 1 : 0
-
+    
     const postUpvoteDownvoteDB: postUpvoteDownvoteDB = {
       post_id: postDB.id,
       user_id: payload.id,
       vote: voteDB
     }
-
+    
     const post = new Post(
       postDB.id,
       postDB.creator_id,
@@ -246,34 +185,101 @@ export class PostBusiness {
       postDB.comments,
       postDB.created_at,
       postDB.updated_at
-    )
-
-    const postUpvoteDownvote = await this.postDatabase.findPostUpvoteDownvote(postUpvoteDownvoteDB)
-
-    //upvote or downvote check 
-    if (postUpvoteDownvote === "up") { //alreary upvoted
-      if (vote) {
-        await this.postDatabase.removeUpvoteDownvote(postUpvoteDownvoteDB)
-        post.removeUpvote()
+      )
+      
+      const postUpvoteDownvote = await this.postDatabase.findPostUpvoteDownvote(postUpvoteDownvoteDB)
+      
+      //upvote or downvote check 
+      if (postUpvoteDownvote === "up") { //alreary upvoted
+        if (vote) {
+          await this.postDatabase.removeUpvoteDownvote(postUpvoteDownvoteDB)
+          post.removeUpvote()
+        } else {
+          await this.postDatabase.updateUpvoteDownvote(postUpvoteDownvoteDB)
+          post.removeUpvote()
+          post.addDownvote()
+        }
+      } else if (postUpvoteDownvote === "down") { //alreary downvoted
+        if (vote) {
+          await this.postDatabase.updateUpvoteDownvote(postUpvoteDownvoteDB)
+          post.removeDownvote()
+          post.addUpvote()
+        } else {
+          await this.postDatabase.removeUpvoteDownvote(postUpvoteDownvoteDB)
+          post.removeDownvote()
+        }
       } else {
-        await this.postDatabase.updateUpvoteDownvote(postUpvoteDownvoteDB)
-        post.removeUpvote()
-        post.addDownvote()
+        await this.postDatabase.upvoteDownvotePost(postUpvoteDownvoteDB)
+        voteDB ? post.addUpvote() : post.addDownvote()
       }
-    } else if (postUpvoteDownvote === "down") { //alreary downvoted
-      if (vote) {
-        await this.postDatabase.updateUpvoteDownvote(postUpvoteDownvoteDB)
-        post.removeDownvote()
-        post.addUpvote()
-      } else {
-        await this.postDatabase.removeUpvoteDownvote(postUpvoteDownvoteDB)
-        post.removeDownvote()
-      }
-    } else {
-      await this.postDatabase.upvoteDownvotePost(postUpvoteDownvoteDB)
-      voteDB ? post.addUpvote() : post.addDownvote()
+      
+      await this.postDatabase.updatePost(idToVote, post.toDBModel())
     }
 
-    await this.postDatabase.updatePost(idToVote, post.toDBModel())
+    
+    //=====================================
+    //ENDPOINTS NOT USED IN MOBILE VERSION
+    //=====================================
+
+    // public editPost = async (input: EditPostOutputDTO): Promise<void> => {
+    //   const { idToEdit, token, content } = input
+  
+    //   const postDB = await this.postDatabase.getPostById(idToEdit)
+    //   if (!postDB) {
+    //     throw new NotFoundError("ERROR: 'idToEdit' not found.")
+    //   }
+  
+    //   //login ckeck
+    //   const payload = this.tokenManager.getPayload(token)
+    //   if (payload === null) {
+    //     throw new BadRequestError("ERROR: Login failed")
+    //   }
+  
+    //   if (postDB.creator_id !== payload.id) {
+    //     throw new ForbiddenError("ERROR: There's no permission to complete the request.")
+    //   }
+  
+    //   //characters quantity
+    //   if (content.length > 280) {
+    //     throw new BadRequestError("ERROR: The maximum post length is 280 characters.")
+    //   }
+  
+    //   const newPost = new Post(
+    //     postDB.id,
+    //     postDB.creator_id,
+    //     postDB.content,
+    //     postDB.upvote,
+    //     postDB.downvote,
+    //     postDB.comments,
+    //     postDB.created_at,
+    //     postDB.updated_at
+    //   )
+  
+    //   newPost.setContent(content)
+    //   newPost.setUpdatedAt(new Date().toISOString())
+  
+    //   await this.postDatabase.updatePost(idToEdit, newPost.toDBModel())
+    // }
+  
+    // public deletePost = async (input: DeletePostOutputDTO): Promise<void> => {
+    //   const { idToDelete, token } = input
+  
+    //   const postDB: PostDB | undefined = await this.postDatabase.getPostById(idToDelete)
+    //   if (!postDB) {
+    //     throw new NotFoundError("ERROR: 'idToDelete' not found.")
+    //   }
+  
+    //   //login ckeck
+    //   const payload = this.tokenManager.getPayload(token)
+    //   if (payload === null) {
+    //     throw new BadRequestError("ERROR: Login failed.")
+    //   }
+  
+    //   //permission check
+    //   if (payload.role !== USER_ROLES.ADMIN && postDB.creator_id !== payload.id) {
+    //     throw new ForbiddenError("ERROR: There's no permission to complete the request.")
+    //   }
+  
+    //   await this.postDatabase.deletePost(idToDelete)
+    // }
   }
-}
